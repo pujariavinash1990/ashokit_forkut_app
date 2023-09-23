db2e97321b9be6d749bf420eda2abedf4fb4f364
Avinash <pujariavinash1990@gmail.com>
2023-09-23 15:34:45 +0530

    moodify the java class

--- src/main/java/in/ashokit/Welcome.java
+++ src/main/java/in/ashokit/Welcome.java
@@ -2,7 +2,7 @@ package in.ashokit;
 
 public class Welcome {
 	public static void main(String[] args) {
-		System.out.println("Welcome to git Hub");
+		System.out.println("Welcome to git Hub now work with git hubs");
 	}
 
 }
