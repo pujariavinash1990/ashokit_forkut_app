a9941f573053436e2120aaaa9e7ee6570a78350e
Avinash <pujariavinash1990@gmail.com>
2023-09-27 16:51:32 +0530

    updated welcome file

--- src/main/java/in/ashokit/Welcome.java
+++ src/main/java/in/ashokit/Welcome.java
@@ -3,6 +3,9 @@ package in.ashokit;
 public class Welcome {
 	public static void main(String[] args) {
 		System.out.println("Welcome Ashokit");
+			int i=20;
+			int j=30;
+		System.out.println("Addition is :"+(i+j));
 	}
 
 }
